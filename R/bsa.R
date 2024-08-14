7c3b2747e8935221153a0a69e771965db12ecb6e
Shirley Liu <xl394@cam.ac.uk>
2024-08-14 11:59:58 +0100

    Use glmnet::glmnet instead of glmnet

--- R/bsa.R
+++ R/bsa.R
@@ -38,16 +38,16 @@ bsachgpt<-function(X,Y,kmax,c1,m1,delta){
         betahat33<-matrix(0,nrow =(alpha[i+1]-m1),ncol = p)
         betahat55<-matrix(0,nrow =(alpha[i+1]-m1),ncol = p)
         
-        betahat22<-as.vector(coef(glmnet(X[alpha[i]:s,],Y[alpha[i]:s],intercept=FALSE,
+        betahat22<-as.vector(coef(glmnet::glmnet(X[alpha[i]:s,],Y[alpha[i]:s],intercept=FALSE,
                                          family="binomial"),s=c1*(sqrt(2*log(2*p)/(s-alpha[i]+1))+log(2*p)/(s-alpha[i]+1)))) # the estimation of regression coefficients
         betahat33[s,]<-betahat22[-1]
-        betahat44<-as.vector(coef(glmnet(X[(s+1):(alpha[i+1]),],Y[(s+1):(alpha[i+1])],
+        betahat44<-as.vector(coef(glmnet::glmnet(X[(s+1):(alpha[i+1]),],Y[(s+1):(alpha[i+1])],
                                          intercept=FALSE,family="binomial"),
                                   s=c1*(sqrt(2*log(2*p)/(alpha[i+1]-s))+log(2*p)/(alpha[i+1]-s))))
         betahat55[s,]<-betahat44[-1]
         
         I<-rep(1,(alpha[i+1]-alpha[i]+1))
-        betahat66<-as.vector(coef(glmnet(X[alpha[i]:alpha[i+1],],Y[alpha[i]:alpha[i+1]],
+        betahat66<-as.vector(coef(glmnet::glmnet(X[alpha[i]:alpha[i+1],],Y[alpha[i]:alpha[i+1]],
                                          intercept=FALSE,family="binomial"),s=c1*(sqrt(2*log(2*p)/(alpha[i+1]-alpha[i]+1))+log(2*p)/(alpha[i+1]-alpha[i]+1))))   # the estimation of regression coefficients
         
         hs[alpha[i]]<-sum(log(I+exp(X[alpha[i]:alpha[i+1],]%*%betahat66[-1]))-Y[alpha[i]:alpha[i+1]]*(X[alpha[i]:alpha[i+1],]%*%betahat66[-1]))/n
