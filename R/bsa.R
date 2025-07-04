c2e2cb11b43b1a8bebe9a29e570a98907af593ce
Gabriel Arpino <arpino.gabriel@gmail.com>
2025-07-04 19:23:57 -0400

    correct syntax

--- R/bsa.R
+++ R/bsa.R
@@ -148,10 +148,14 @@ bsa_exactchgpt<-function(X,Y,kmax,c1,m1,delta){
   # Default is 100. Reducing it might resolve issues if 100 is too high for certain p/n.
   # pmax: maximum number of variables ever to be nonzero. Default is p.
   # Explicitly setting it to p is generally safe and robust.
-  glmnet_nlambda <- 10 # Standard default, can be reduced if needed (e.g., 50 or 20)
-  glmnet_pmax <- min(p, 50)     # Explicitly set to the number of features 'p'
+  # glmnet_nlambda <- 10 # Standard default, can be reduced if needed (e.g., 50 or 20)
+  # glmnet_pmax <- min(p, 50)     # Explicitly set to the number of features 'p'
+  # In bsa_exactchgpt function, replace the parameter section with:
+  glmnet_nlambda <- 2   # Very conservative
+  glmnet_pmax <- 10     # Very conservative
+  print(paste("updated 4: glmnet_pmax is set to:", glmnet_pmax))
+  print(paste("updated 4: glmnet_nlambda is set to:", glmnet_nlambda))
   # Print what glmnet_pmax is
-  print(paste("updated 2: glmnet_pmax is set to:", glmnet_pmax))
   # --- END ADDITION ---
 
   # The for loop below is the only difference between this function
